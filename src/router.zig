1b14375147b316310de717700008777bd7081580
caleberi <caleberioluwa@gmail.com>
2024-12-05 00:14:03 +0100

    feat: move routes to a single file

--- src/router.zig
+++ src/router.zig
@@ -1,62 +1,16 @@
 const zzz = @import("zzz");
+const std = @import("std");
+const Agent = @import("./ddog/agent.zig");
 const zhttp = zzz.HTTP;
 const Server = zhttp.Server(.plain);
 const Router = Server.Router;
 const Route = Server.Route;
 const Context = Server.Context;
-const std = @import("std");
-const Agent = @import("./ddog/agent.zig");
-const Trace = Agent.Trace;
-const GenericBatchWriter = @import("./ddog/batcher.zig").GenericBatchWriter;
-pub const Dependencies = struct {
-    env: *std.process.EnvMap,
-    ddog: *Agent.DataDogClient,
-    tracer: *GenericBatchWriter(Trace),
-};
+const Handlers = @import("handlers.zig");
+const Dependencies = Handlers.Dependencies;
 
 pub fn bindRoutes(router: *Router, deps: *Dependencies) !void {
-    try router.serve_route("/kill", Route.init().get({}, kill_fn));
-    try router.serve_route("/", Route.init().post(deps, test_fn));
-}
-
-fn kill_fn(ctx: *Context, _: void) !void {
-    ctx.runtime.stop();
-    std.process.exit(0);
-}
-
-fn test_fn(ctx: *Context, deps: *Dependencies) !void {
-    if (!std.mem.eql(u8, ctx.request.headers.get("content-type").?, "application/json")) {
-        return ctx.respond(.{ .status = .@"Bad Request", .mime = zhttp.Mime.JSON, .body = null });
-    }
-
-    const data = ctx.allocator.dupe(u8, ctx.request.body) catch return ctx.response.set(.{ .status = .OK, .mime = zhttp.Mime.JSON, .body = "something went wrong" });
-    defer ctx.allocator.free(data);
-
-    const parsed_trace: std.json.Parsed(Trace) = std.json.parseFromSlice(Trace, ctx.allocator, data, .{ .ignore_unknown_fields = true }) catch return ctx.respond(.{
-        .status = .OK,
-        .mime = zhttp.Mime.HTML,
-        .body = "error passing json",
-    });
-
-    defer parsed_trace.deinit();
-    const trace = parsed_trace.value;
-    const ddog: *Agent.DataDogClient = deps.ddog;
-    const tracer: *GenericBatchWriter(Agent.Trace) = deps.tracer;
-
-    const result: Agent.Result = ddog.sendTrace(trace, .{
-        .batched = true,
-        .batcher = tracer,
-        .compressible = true,
-        .compression_type = .{ .level = .best },
-    }) catch return ctx.respond(.{
-        .status = .OK,
-        .mime = zhttp.Mime.HTML,
-        .body = "cannot send trace",
-    });
-
-    return ctx.respond(.{
-        .status = .OK,
-        .mime = zhttp.Mime.TEXT,
-        .body = result.@"0",
-    });
+    try router.serve_route("/trace", Route.init().post(deps, Handlers.TraceHandler));
+    try router.serve_route("/metric", Route.init().post(deps, Handlers.TraceHandler));
+    try router.serve_route("/log", Route.init().post(deps, Handlers.TraceHandler));
 }
