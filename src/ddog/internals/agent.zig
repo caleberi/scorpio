1a7b57013878da9a8d53a8f5c1a5168d7e0c4c41
caleberi <caleberioluwa@gmail.com>
2024-12-12 22:16:28 +0100

    fix : agent client with sendtrace

--- src/ddog/internals/agent.zig
+++ src/ddog/internals/agent.zig
@@ -1,10 +1,6 @@
 const std = @import("std");
-const net = std.net;
 const json = std.json;
 const http = std.http;
-const Runtime = @import("tardy").Runtime;
-const Task = @import("tardy").Task;
-const Allocator = std.mem.Allocator;
 const AgentLogger = @import("../features/log.zig");
 const AgentTracer = @import("../features/trace.zig");
 const AgentMetricRecorder = @import("../features/metric.zig");
@@ -18,44 +14,44 @@ const Metric = Features.metric.Metric;
 const Trace = Features.trace.Trace;
 const TraceSubmissionOptions = Features.trace.TraceSubmissionOptions;
 
-pub const Tardy = @import("tardy").Tardy(.auto);
 pub const Result = std.meta.Tuple(&[_]type{ []const u8, ?StatusError });
 
 pub const DdogClient = struct {
     api_key: []const u8,
     host: []const u8,
-    allocator: Allocator,
-    http_client: ?*http.Client = null,
+    allocator: std.mem.Allocator,
+    http_client: *http.Client,
     const Self = @This();
 
-    pub fn init(allocator: Allocator, api_key: []const u8, api_site: []const u8) !Self {
+    pub fn init(allocator: std.mem.Allocator, api_key: []const u8, api_site: []const u8) !Self {
         const buffer = try allocator.alloc(u8, 100);
-        const client = try allocator.create(http.Client);
         defer allocator.free(buffer);
         const site: []u8 = try std.fmt.bufPrint(buffer, "https://api.{s}", .{api_site});
+        const client = try allocator.create(http.Client);
+        client.* = http.Client{ .allocator = allocator };
         return .{
             .api_key = api_key,
-            .host = try allocator.dupe(u8, site),
             .allocator = allocator,
+            .host = try allocator.dupe(u8, site),
             .http_client = client,
         };
     }
 
     pub fn deinit(self: *Self) void {
-        self.http_client.?.deinit();
-        self.allocator.destroy(self.http_client.?);
-        self.allocator.free(self.host);
+        defer self.allocator.free(self.host);
+        defer self.allocator.destroy(self.http_client);
+        self.http_client.deinit();
     }
 
-    // pub fn submitLog(self: *Self, log: Log, opts: LogOpts) !Result {
-    //     return try @call(.auto, AgentLogger.submitLog, .{ self, log, opts });
-    // }
+    pub fn submitLog(self: *Self, logs: []Log, opts: LogOpts) !Result {
+        return try @call(.auto, AgentLogger.submitLogs, .{ self, logs, opts });
+    }
 
     // pub fn submitMetric(self: *DdogClient, metric: Metric) !Result {
     //     return try @call(.auto, AgentMetricRecorder.submitMetric, .{ self, metric });
     // }
 
-    pub fn sendTrace(self: *DdogClient, trace: [][]Trace, opts: TraceSubmissionOptions) !Result {
+    pub fn sendTrace(self: *Self, trace: []Trace, opts: TraceSubmissionOptions) !Result {
         return try @call(.auto, AgentTracer.submitTraces, .{ self, trace, opts });
     }
 };
