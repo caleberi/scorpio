38f39b82fb9b9f586ef08568fe434b5b860c959c
Caleb Adewole Erioluwa <caleberioluwa@gmail.com>
2026-08-15 09:54:08 +0100

    fix: uncomment JSON import and test in root.zig

--- common/root.zig
+++ common/root.zig
@@ -1,11 +1,11 @@
 pub const utils = @import("utils.zig");
 pub const status = @import("status.zig");
 pub const mp = @import("mp.zig");
-// pub const json = @import("json.zig");
+pub const json = @import("json.zig");
 
 test {
     _ = utils;
     _ = status;
     _ = mp;
-    // _ = json;
+    _ = json;
 }
